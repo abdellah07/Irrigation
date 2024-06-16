3a654919350e81c8dcd734b5b8062362cd13dc5d
Abdellah07 <abdellah0naji@gmail.com>
2024-06-16 21:14:01 +0200

    first commit

--- src/main/java/com/naji/gmail/irrigation/Controller/IrrigationController.java
+++ src/main/java/com/naji/gmail/irrigation/Controller/IrrigationController.java
@@ -1,2 +1,15 @@
-package com.naji.gmail.irrigation.Controller;public class IrrigationController {
+package com.naji.gmail.irrigation.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class IrrigationController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "hello world !!";
+    }
 }
--- src/main/java/com/naji/gmail/irrigation/IrrigationApplication.java
+++ src/main/java/com/naji/gmail/irrigation/IrrigationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IrrigationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IrrigationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IrrigationApplication.class, args);
+    }
 
 }
